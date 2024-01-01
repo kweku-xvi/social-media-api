3ad123a80bcdc8148794dfae1b1671d52f0b0bdd
kweku-xvi <nksarps@gmail.com>
2024-01-01 12:53:05 +0000

    add account verification and password reset email senders

---
+++ accounts/utils.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from trycourier import Courier
+
+load_dotenv()
+
+client = Courier(auth_token=os.getenv('AUTH_TOKEN'))
+
+def send_verification_email(email:str, username:str, link:str):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": "HQRFKDHDK84B16GJAQ7PWPFATXS8",
+            "data": {
+            "username": username,
+            "link": link,
+            },
+        }
+    )
+
+def send_password_reset_mail(email:str, username:str, link:str):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": "WF7909Y7ZWMNWNNTNNQRHDTBKDF4",
+            "data": {
+            "username": username,
+            "link": link,
+            },
+        }
+    )
