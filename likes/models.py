6017b5b0485fc41e05507d9f2c0a66b4b4f718d6
kweku-xvi <nksarps@gmail.com>
2024-01-04 10:22:37 +0000

    add models for liking posts and comments

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,28 @@
+from accounts.models import User
+from comments.models import Comment
 from django.db import models
+from posts.models import Post
 
-# Create your models here.
+
+class LikePost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_liked = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'User - {self.user.username} - Liked - Post - {self.post.text}'
+
+    class Meta():
+        ordering = ('-time_liked',)
+
+
+class LikeComment(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_liked = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'User - {self.user.username} - Liked - Comment - {self.comment.comment}'
+
+    class Meta():
+        ordering = ('-time_liked',)
