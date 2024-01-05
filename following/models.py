050fbe5c9a0a778f20187318dcfbf64991fbe8b6
kweku-xvi <nksarps@gmail.com>
2024-01-05 09:33:32 +0000

    add model to follow and unfollow users

--- following/models.py
+++ following/models.py
@@ -1,3 +1,13 @@
+from accounts.models import User
 from django.db import models
 
-# Create your models here.
+class FollowUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    time_followed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.follower.username} follows {self.user.username}'
+
+    class Meta:
+        ordering = ('-time_followed',)
